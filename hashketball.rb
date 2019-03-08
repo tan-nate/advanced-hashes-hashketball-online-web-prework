175afb12cc5d3c5df8b3c12fa038cd1d82578771
Nate Tan <nktan93@gmail.com>
2019-03-08 22:27:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -172,7 +172,7 @@ def big_shoe_rebounds
   
   
   players = game_hash.collect do |team, value|
-    value[:players].keys.flatten.flatten
+    value[:players].keys
   end
   
   sorted_shoes = shoes.flatten.sort
